3c32d22da0f9c5a258b96298f30a53fc949ef253
Terje Pedersen <terje@senikk.com>
2011-10-14 22:25:53 +0200

    added ref to secrets

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 ﻿require 'sinatra'
 require File.dirname(__FILE__) + '/test.rb'
 require File.dirname(__FILE__) + '/test2.rb'
+require File.dirname(__FILE__) + '/secrets.rb'
 
 #set :raise_errors, true
 
 get '/' do
-  "Simpelt som bare det!"
+  "Simpelt som bare det 2!"
 end
 
 get '/test' do
